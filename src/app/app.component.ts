4c253b41a0e8856c1871546af49fffa860d721b9
Kerschbaumer Stefan <xsip@pm.me>
2023-04-06 11:35:00 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,28 +2,54 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+      <div class="w-full flex mx-auto p-10 bg-[rgb(253,245,241)] text-[rgb(128,131,141)] max-w-7xl mt-auto h-screen">
+          <div class="flex rounded-tl-md rounded-bl-md flex-col justify-between w-[30%] drop-shadow-xl  border-2 p-10 pr-10 bg-[rgb(240,240,240)]">
+              <div class="">
+                  <img alt="profile" class="w-[100%] rounded-md object-contain h-auto drop-shadow-md"
+                       src="../assets/profile.jpg"/>
+                  <h1 class="text-xl mt-5 font-bold">Kerschbaumer Stefan</h1>
+                  <h1 class="text-xl font-bold">Fullstack Developer</h1>
+                  <h1 class="text-md break-words ">Angular, NodeJS, Typescript, NestJS, Javascript,HTML,SCSS,CSS,
+                      GraphQL, Rxjs,Ngrx</h1>
+
+              </div>
+              <div>
+                  <div class="flex gap-2 flex-col md:flex-col">
+                    <div class="flex items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                           stroke="currentColor" class="w-4 h-4">
+                          <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"/>
+                      </svg>
+                      <p class="text-sm">Email </p>
+                    </div>
+                    <div class="flex  items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                           stroke="currentColor" class="w-4 h-4">
+                          <path stroke-linecap="round"
+                                d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"/>
+                      </svg>
+                      <p class="text-sm">Anrufen</p>
+                    </div>
+
+                  </div>
+              </div>
+          </div>
+          <div class="flex w-[70%] relative rounded-tr-md rounded-br-md drop-shadow-md bg-white pt-20 pl-2 pr-2 border-2">
+              <nav class="fixed flex flex-col items-center justify-center top-0 left-0 w-full h-16 bg-white drop-shadow-md">
+                  <ul class="flex gap-6">
+                      <li class="hover:scale-125 transition-all text-md cursor-pointer">Über mich</li>
+                      <li class="hover:scale-125 transition-all text-md cursor-pointer">Werdegang</li>
+                      <li class="hover:scale-125 transition-all text-md cursor-pointer">Projekte</li>
+                  </ul>
+              </nav>
+            <p class="px-10">
+              Mein Name ist Kerschbaumer Stefan und ich bn seit mittlerweile Sechs Jahren als Fullstack Developer Tätig. Ich arbeite im Frontend hauptsächlich mit Angular - beherrsche allerdings auch nexts - und entwickle Backend Applikationen mit NestJS, Express oderNodeJS. Außerdem beherrsche ich typescript sehr gut, habe Kenntnisse in GraphQL, rxjs, ngrx und kann mit Sql, sowie NoSql( MongoDB ) Datenbanken umgehen.
+            </p>
+          </div>
+      </div>
   `,
   styles: []
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { CommonModule } from '@angular/common';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,{
+  providers: [importProvidersFrom(CommonModule, BrowserModule)],
+}).catch(err => console.error(err));

5c57f69ed47d9f63b17d7895744db62469c8f033
Kerschbaumer Stefan <xsip@pm.me>
2023-04-07 16:11:57 +0200

    improves darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,16 @@ import {FormsModule} from '@angular/forms';
   template: `
       <div class="bg-[rgb(253,245,241)] w-full h-screen dark:bg-gradient-to-r dark:from-slate-700 dark:to-slate-800">
           <div class="w-full flex md:flex-row flex-col mx-0 md:mx-auto md:p-10  text-[rgb(128,131,141)] md:max-w-7xl mt-auto h-screen">
-              <div class="flex rounded-tl-md rounded-bl-md flex-col justify-between w-[100%] md:w-[30%] drop-shadow-xl   p-10 pr-10 bg-[rgb(240,240,240)]">
+              <div class="flex rounded-tl-md rounded-bl-md flex-col justify-between w-[100%] md:w-[30%] drop-shadow-xl   p-10 pr-10 dark:bg-slate-600  dark:text-white bg-[rgb(240,240,240)]">
                   <div class="">
-                      <img alt="profile" class="w-[100%] rounded-md object-contain h-auto drop-shadow-md"
+                      <img *ngIf="!isDarkMode(); else lightpic" alt="profile"
+                           class="w-[100%] rounded-md object-contain h-auto drop-shadow-md"
                            src="../assets/profile.jpg"/>
+                      <ng-template #lightpic>
+                          <img alt="profile" class=" w-[100%] rounded-md object-contain h-auto drop-shadow-md"
+                               src="../assets/profile-dark.jpg"/>
+                      </ng-template>
+
                       <h1 class="text-xl mt-5 font-bold">Kerschbaumer Stefan</h1>
                       <h1 class="text-xl font-bold">Fullstack Developer</h1>
                       <h1 class="text-md break-words ">Angular, NodeJS, Typescript, NestJS, Javascript,HTML,SCSS,CSS,
@@ -53,7 +59,12 @@ import {FormsModule} from '@angular/forms';
 
                           <a target="_blank" href="https://www.github.com/xsip"
                              class="flex  items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
-                              <img class="w-4 h-4 opacity-70" src="../assets/github-mark.png"/>
+                              <img *ngIf="!isDarkMode(); else lightIcon" class="w-4 h-4 opacity-70"
+                                   src="../assets/github-mark.png"/>
+                              <ng-template #lightIcon>
+                                  <img class="w-4 h-4 opacity-70"
+                                       src="../assets/github-mark-white.png"/>
+                              </ng-template>
                               <p class="text-sm">Github</p>
                           </a>
 
@@ -83,19 +94,23 @@ import {FormsModule} from '@angular/forms';
                           >darkmode</label
                           >!-->
                       </div>
-                    <div *ngIf="darkMode" (click)="darkModeToggle()" class="z-50 w-5 cursor-pointer hover:scale-110 h-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
-                    </svg>
-                    </div>
-                    <div *ngIf="!darkMode" (click)="darkModeToggle()" class="z-50 w-5 flex cursor-pointer hover:scale-110 h-5">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-                      </svg>
+                      <div *ngIf="darkMode" (click)="darkModeToggle()"
+                           class="z-50 w-5 cursor-pointer hover:scale-110 h-5">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                               stroke="currentColor" className="w-6 h-6">
+                              <path strokeLinecap="round" strokeLinejoin="round"
+                                    d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
+                          </svg>
+                      </div>
+                      <div *ngIf="!darkMode" (click)="darkModeToggle()"
+                           class="z-50 w-5 flex cursor-pointer hover:scale-110 h-5">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                               stroke="currentColor" class="w-6 h-6">
+                              <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"/>
+                          </svg>
 
-                    </div>
+                      </div>
                   </nav>
                   <div id="werdegang" class="px-10 pt-10 pb-10">
                       <h1 class="text-2xl font-bold pb-2">Werdegang</h1>
@@ -172,6 +187,7 @@ export class AppComponent implements OnInit{
     this.darkMode = !this.darkMode;
     this.darkModeChange();
   }
+  isDarkMode = () => document.body.classList.contains('dark');
   darkModeChange() {
     localStorage.setItem('dark', JSON.stringify(this.darkMode));
     if(this.darkMode) {

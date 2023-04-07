8d6dc86aae12dee26facb51bdd0733ffb2cca795
Kerschbaumer Stefan <xsip@pm.me>
2023-04-07 09:52:20 +0200

    changes linklist behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,9 @@ import {FormsModule} from '@angular/forms';
 
                   </div>
                   <div>
-                      <div class="flex gap-2 flex-col md:flex-col">
+                      <div class="gap-2 flex-row mt-2 flex md:inline-block">
                           <a href="tel:+4369911070940"
-                             class="flex items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
+                             class=" md:pb-1 flex items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                    stroke="currentColor" class="w-4 h-4">
                                   <path stroke-linecap="round" stroke-linejoin="round"
@@ -32,7 +32,7 @@ import {FormsModule} from '@angular/forms';
                               <p class="text-sm">Anrufen </p>
                           </a>
                           <a href="mailto:xsip@pm.me"
-                             class="flex  items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
+                             class="flex md:pb-1  w-auto items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                    stroke="currentColor" class="w-4 h-4">
                                   <path stroke-linecap="round"
@@ -41,7 +41,7 @@ import {FormsModule} from '@angular/forms';
                               <p class="text-sm">Email</p>
                           </a>
                         <a href="../assets/cv.pdf" target="_blank"
-                             class="flex  items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
+                             class="flex  md:pb-1 items-center gap-1 cursor-pointer hover:scale-110 md:hover:scale-110 transition-all">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                           </svg>
